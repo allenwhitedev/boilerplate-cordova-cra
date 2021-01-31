8ffb0d48833d99567ecf621bae3dad368599bf1a
aw <aw@aws-MacBook-Pro.local>
2021-01-31 18:22:39 -0500

    Get cordova to run on android device (with cordAndroidD) and ios simulator with cordova run ios

---
+++ www/precache-manifest.fbf9a49e53aa44638bcc638843f00d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e57a73cbe95867445cbc2bea6ccf19b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed7ad8a1640720ddb74f",
+    "url": "./static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "22101ae71f0a44125474",
+    "url": "./static/js/2.011e34c4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.011e34c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7ad8a1640720ddb74f",
+    "url": "./static/js/main.ac1e32d6.chunk.js"
+  },
+  {
+    "revision": "1b5c148b9b0327f3781e",
+    "url": "./static/js/runtime-main.df6ee5cc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
